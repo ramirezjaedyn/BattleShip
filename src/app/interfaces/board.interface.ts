558beb8a1ed449583c3d9b976c3ec1b25f269b60
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-11-10 11:35:05 -0600

    adds board interface

---
+++ src/app/interfaces/board.interface.ts
@@ -0,0 +1,12 @@
+export interface Board{
+    a: Array<number>;
+    b: Array<number>;
+    c: Array<number>;
+    d: Array<number>;
+    e: Array<number>;
+    f: Array<number>;
+    g: Array<number>;
+    h: Array<number>;
+    i: Array<number>;
+    j: Array<number>;
+    }
