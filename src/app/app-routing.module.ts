1d2dda9ace081d32a4c1105cea5aec4da097b5d4
Monica Sweenie <monica@monica-sweenies-computer.local>
2020-11-10 12:27:42 -0600

    Adding google login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { GameComponent } from './game/game.component';
+import{AngularFireAuthGuard } from'@angular/fire/auth-guard';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent, canActivate: [AngularFireAuthGuard] },
   {path: 'game/:gameId', component: GameComponent},
   {path: '**', redirectTo: '/'}
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+//import { auth } from "firebase";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, private router: Router) { }
 
+  
   ngOnInit(): void {
   }
 
+  
+
+  login() {
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(
+      val => this.router.navigate([''])
+    ).catch(err => this.router.navigate(['/'])
+  )};
+  
+
+
+  logout(){
+    this.auth.signOut().then(val => {
+      this.router.navigate(['/'])
+    }).catch()
+  }
+      
 }
+
+
