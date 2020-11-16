cbf71d550c3351be0da82bac28ed20e65583a192
ALEC_YAKSICH <ALEC_YAKSICH@midlandu.edu>
2020-11-16 16:57:59 -0600

    server.js,service,and config finished for sockets

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { GameService } from 'battleship-c5/src/app/services/game.service';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
+  socket: any;
+  displayName: string;
+  constructor(private afs: AngularFirestore, private router: Router, private gameservice: GameService, private auth: AngularFireAuth) {
 
-  constructor() { }
+    this.auth.user.subscribe(v=> {
+      this.displayName = v ? v.displayName : null;
+    });
+
+    this.socket = io.connect();
+    this.socket.on('joinGame', (displayName, gameId) => {
+      displayName = displayName;
+      gameId = gameId;
+    })
+
+    this.socket.on('gameEnd', (gameId) => {
+      this.socket.on('roomClosed', () => {
+        this.router.navigate(["/home"])
+      })
+    })
+  }
+  public get chatMessage$() {
+    return Observable.create((observer) => {
+      this.socket.on('newMessage', (message) => {
+        observer.next(message);
+      })
+    })
+  }
+
+  // Join game function
+  joinGame(displayName: string, gameId: string) {
+    this.socket.emit('joinGame', displayName, gameId);
+  }
+
+  // Leave game function
+  leaveGame(displayName: string, gameId: string) {
+    this.socket.emit('leaveGame', displayName, gameId);
+  }
+
+  sendChat(msg: string){
+    this.socket.emit('newMessage', {msg: msg, displayName: this.displayName})
+  }
+
+  // Create game function to setup host socket
+
+  roomClosed() {
+    this.socket.emit('roomClosed')
+  }
 }
