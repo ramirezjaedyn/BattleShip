abe9e34a7447781278f2debe6c0c52d3e9807609
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-11-17 14:09:21 -0600

    adds some basic status notes

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-game',
@@ -9,10 +12,27 @@ export class GameComponent implements OnInit {
 
   // Check for who the logged in user is
   userId: string;
-  
-  // enemy userId pulled from firestore
-  // In template, you would pass the userId to the board (so you can pull only that specific board)
-  constructor() { }
+  gameInfo: any = null; // houses game data
+  playersTurn: string = "";
+
+  constructor(private afs: AngularFirestore, private gameService: GameService, private auth: AngularFireAuth) {
+    this.gameInfo = this.gameService.gameInfo;
+    this.playersTurn = this.gameInfo.activePlayer ;
+    
+    this.auth.user.subscribe(v=> {
+      this.userId = v ? v.uid : null;
+      if (this.userId == null){
+        this.playersTurn= "Waiting for game to be ready..."
+      }
+      else if (this.userId === this.playersTurn) {
+        this.playersTurn = "User"
+      }
+      else {
+        this.playersTurn = "Opponent"
+      }
+    });
+
+   }
 
 
   ngOnInit(): void {
