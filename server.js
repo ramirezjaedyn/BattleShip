2d84777671557d7961786ab61269e9c34b8eb925
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-11-17 15:14:18 -0600

    fixes typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 const server = require('http').Server(app);
-const io = require('./server/config/socket.conf').listen(server);
+const io = require('./server/config/sockets.conf').listen(server);
 
 app.use(express.static(__dirname + "/dist"));
 
