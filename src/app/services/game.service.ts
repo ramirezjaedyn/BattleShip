361b50ae1a9ce2a45785169ad86d28ef5903195e
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-11-18 09:38:36 -0600

    adds deleteGame functionality

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -49,7 +49,7 @@ export class GameService {
    * Deletes the AngularFireStore game information via the game's gameId
    */
   deleteGame() {
-
+    this.afs.collection('game').doc(`${this.gameId}`).delete();
   }
 
 
