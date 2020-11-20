53c39e16f9ba17b9f5e8d53d83c8a97e06af4fd8
Jaedyn Ramirez <jaedynramirez@jaedyns-mbp.mlc.edu>
2020-11-20 09:22:17 -0600

    fixes dialog close button

--- src/app/instructions-dialog/instructions-dialog.component.ts
+++ src/app/instructions-dialog/instructions-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-instructions-dialog',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstructionsDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
