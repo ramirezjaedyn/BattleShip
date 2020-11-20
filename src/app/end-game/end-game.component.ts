ad54d4b122e3bc1b97b0fd11fddd4601925bdc07
Garrett Sloup <garrettsloup@macbook-pro.mlc.edu>
2020-11-20 09:52:31 -0600

    fixes so both dialogs pop up

--- src/app/end-game/end-game.component.ts
+++ src/app/end-game/end-game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { MatDialogRef } from '@angular/material/dialog';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-end-game',
@@ -8,15 +10,27 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class EndGameComponent implements OnInit {
 
-  winner: string = "John"
+  winner: string;
+  userId: string;
 
-  constructor(public dialogRef: MatDialogRef<EndGameComponent>) { }
+  constructor(public dialogRef: MatDialogRef<EndGameComponent>, private gameService: GameService, private auth: AngularFireAuth) { 
+    this.auth.user.subscribe(v => {
+      this.userId = v ? v.uid : null;
+    });
+  }
 
   quitGame(){
     this.dialogRef.close();
   }
 
   ngOnInit(): void {
+    let winnerId = this.gameService.gameInfo.activePlayer;
+    if (winnerId = this.userId) {
+      this.winner = "You"
+    }
+    else {
+      this.winner = "Opponent"
+    }
   }
 
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EndGameComponent } from '../end-game/end-game.component';
 import { GameService } from '../services/game.service';
 
 @Component({
@@ -15,13 +17,21 @@ export class GameComponent implements OnInit {
   userId: string;
   gameInfo: any = null; // houses game data
 
-  constructor(private afs: AngularFirestore, private gameService: GameService, private auth: AngularFireAuth, private actr: ActivatedRoute) {
+  constructor(private afs: AngularFirestore, private gameService: GameService, private auth: AngularFireAuth, 
+    private actr: ActivatedRoute, public dialog: MatDialog, private router: Router) {
     this.afs.collection('game').doc(`${this.actr.snapshot.params.gameId}`).valueChanges().subscribe(val=> {
       this.gameInfo = val ?  val: null;
+      if (this.gameInfo.gameOver){
+        this.showWinner();
+      }
     });
     this.auth.user.subscribe(v=> this.userId = v ? v.uid : null);
    }
 
+  showWinner() {
+    const dialogRef = this.dialog.open(EndGameComponent);
+    dialogRef.afterClosed().subscribe(v=> this.router.navigate(['/home']));
+  }
 
   ngOnInit(): void {
   }
